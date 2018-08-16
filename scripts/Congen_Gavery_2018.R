aa01c5dbb1d286248e80edf54bc8856824bc7e40
sr320 <roberts.sbr@gmail.com>
2018-08-16 08:07:00 -0700

    modified R script - making file location relative etc

--- scripts/Congen_Gavery_2018.R
+++ scripts/Congen_Gavery_2018.R
@@ -3,7 +3,7 @@ library(reshape2)
 
 #STEP 1. PREPARING A BED FILE#########################################################################################
 ##Read in the methylKit output file containing DMRs between sperm and RBCs in O.mykiss##
-DMRoutput <- read.csv("/Users/mackenzie.gavery/Desktop/Congen_2018/SpvRBC_DMR100bp_75percdiff")
+DMRoutput <- read.csv("data/SpvRBC_DMR100bp_75percdiff")
 
 #look at the output file
 head(DMRoutput)
@@ -23,46 +23,64 @@ DMRoutput_hypo.bed <- DMRoutput_hypo[,c(2,3,4,8)]
 head(DMRoutput_hypo.bed)
 
 ##save the file as a bed
-write.table(DMRoutput_hypo.bed,"/Users/mackenzie.gavery/Desktop/Congen_2018/DMRs_hypo.bed",col.names = F, sep = '\t', row.names = F,quote = F)
+write.table(DMRoutput_hypo.bed,"analyses/DMRs_hypo.bed",col.names = F, sep = '\t', row.names = F,quote = F)
 
 
 #STEP 2. USING GENOMIC ARITHMATIC TO ANNOTATE REGIONS OF INTEREST#####################################################
 
+#define location of bedtools program on you computer
+bedtools <- "/Applications/bioinfo/bedtools2/bin/bedtools"
+
 #define variables for bedtools command
-bedtools <- "/Users/mackenzie.gavery/software/bedtools2/bin/bedtools"
-DMRs <- "/Users/mackenzie.gavery/Desktop/Congen_2018/DMRs_hypo.bed"
-sortedDMRs<- "/Users/mackenzie.gavery/Desktop/Congen_2018/DMRs_hypo.sorted.bed"
-genes_gff <- "/Users/mackenzie.gavery/Desktop/Congen_2018/Omy_mRNA_subset.gff"
-output <- "/Users/mackenzie.gavery/Desktop/Congen_2018/system_output.txt"
+#this all seems confusing 
+#will try something
+#DMRs <- "analyses/DMRs_hypo.bed"
+#sortedDMRs<- "analyses/DMRs_hypo.sorted.bed"
+#genes_gff <- "/Users/mackenzie.gavery/Desktop/Congen_2018/Omy_mRNA_subset.gff"
+#output <- "/Users/mackenzie.gavery/Desktop/Congen_2018/system_output.txt"
 
 #sort the bed using bedtools
-system(paste(bedtools, "sort -i", DMRs, ">", sortedDMRs))
+#system(paste(bedtools, "sort -i", DMRs, ">", sortedDMRs))
+
+#sort the bed using bedtools
+#bedtools is commonly run in bash - the below notation is crazy and confusing
+system(paste(bedtools, "sort -i", "analyses/DMRs_hypo.bed", ">", "analyses/DMRs_hypo.sorted.bed"))
+
 
 #run bedtools 'closest', use -D to report distance from genes
-system(paste(bedtools, "closest -a", sortedDMRs, "-b", genes_gff, "-D b >", output))
+#system(paste(bedtools, "closest -a", sortedDMRs, "-b", genes_gff, "-D b >", output))
+
+system(paste(bedtools, "closest -a", "analyses/DMRs_hypo.sorted.bed", "-b", "data/Omy_mRNA_subset.gff", "-D b >", "analyses/closetgene.txt"))
+
+
 
 #read in the output file 
-annotated <- read.table(output, sep = '\t')
-head(annotated)
+annotated <- read.table("analyses/closetgene.txt", sep = '\t')
+head(annotated) ##suggest bettter name -like dmr-genes
 
 #assign new column IDs
 ##at least V14 which is distance to gene
 
+
+
 #STEP 3. JOINING TABLES AND FILTERING BASED ON DISTANCE AND QUALIY OF ANNOTATION
 #join the gene number to the SwissProt GeneID
-genes_annot <- read.table("/Users/mackenzie.gavery/Desktop/Congen_2018/Omy_BLAST_UniProt.tab", header=T, sep="\t", stringsAsFactors=F, fill=T, quote="", na.strings="", comment.char="")
+genes_annot <- read.table("data/Omy_BLAST_UniProt.tab", header=T, sep="\t", stringsAsFactors=F, fill=T, quote="", na.strings="", comment.char="")
 
 #look at file (this is a BLAST output file for all O. my genes to the UniProt Database)
 head(genes_annot)
 
 #perform a left join to join the output of bedtools to the blast output
-geneID_IPA<-left_join(annotated,genes_annot, by = c("V13" = "Sequence"))
+geneID_IPA <- left_join(annotated,genes_annot, by = c("V13" = "Sequence"))
 
 #look at it
 head(geneID_IPA)
+nrow(geneID_IPA)
 
 #filter the annotations to, 1) genes within 10kb of DMR, 2) and e-value of < 1e-10
-nrow(geneID_IPA)
+
+#THERE is likely a more elegant way to to this in with dplyr
+
 #filter by distance from gene
 annotations_filterA<-filter(geneID_IPA, V14 >= -10000)
 nrow(annotations_filterA)
@@ -83,7 +101,7 @@ annotations_filterC_UNIPROT <- cbind(annotations_filterC, newCols)
 head(annotations_filterC_UNIPROT)
 
 #save full anotation file
-write.table(annotations_filterC_UNIPROT,"/Users/mackenzie.gavery/Desktop/Congen_2018/DMR_fullannotations.txt",sep = "\t",quote = F,row.names = F)
+write.table(annotations_filterC_UNIPROT,"analyses/DMR_fullannotations.txt",sep = "\t",quote = F,row.names = F)
 
 #save a file of UNIPROT_IDs for enrichment analysis
-write.table(annotations_filterC_UNIPROT$UNIPROT_ID,"/Users/mackenzie.gavery/Desktop/Congen_2018/DMR_UNIPROT_ID.txt",sep = "\t",quote = F,row.names = F,col.names = F)
+write.table(annotations_filterC_UNIPROT$UNIPROT_ID, "analyses/DMR_UNIPROT_ID.txt", sep = "\t", quote = F, row.names = F, col.names = F)
